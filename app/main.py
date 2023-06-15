243c50c9ecf2569743bf4007a58b2aa6ca75b211
Jerrold Koh <jerroldfisher@live.com>
2023-06-15 16:58:42 +0800

    App update

---
+++ app/main.py
@@ -0,0 +1,22 @@
+import joblib
+from pathlib import Path
+import os
+import dill
+import re
+
+# folder structure in docker may be different
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+with open(f'{BASE_DIR}/model/trained_pipeline.joblib', 'rb') as f:
+    model = dill.load(f)
+    
+classes = ['Arabic', 'Danish', 'Dutch', 'English', 'French', 'German',
+       'Greek', 'Hindi', 'Italian', 'Kannada', 'Malayalam', 'Portugeese',
+       'Russian', 'Spanish', 'Sweedish', 'Tamil', 'Turkish']
+
+
+def predict_pipeline(text):
+    
+    pred = model.predict([text])
+    return classes[pred[0]]
+
+print(dill.__version__)
