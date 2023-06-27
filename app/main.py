b4a494d4850196be26836f1dd9af542458aa4080
Jerrold Koh <jerroldfisher@live.com>
2023-06-27 15:53:45 +0800

    ML workflow update

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-import joblib
-from pathlib import Path
-import os
-import dill # 0.3.6
-import re
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import predict_pipeline
+from app.model.model import __version__ as model_version
 
-# folder structure in docker may be different
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-with open(f'{BASE_DIR}/model/trained_pipeline.joblib', 'rb') as f:
-    model = dill.load(f)
+app = FastAPI()
+
+class TextIn(BaseModel):
+    text: str
     
-classes = ['Arabic', 'Danish', 'Dutch', 'English', 'French', 'German',
-       'Greek', 'Hindi', 'Italian', 'Kannada', 'Malayalam', 'Portugeese',
-       'Russian', 'Spanish', 'Sweedish', 'Tamil', 'Turkish']
+    
+class PredictionOut(BaseModel):
+    language: str
+    
+    
+@app.get("/")
+def home():
+    return {"health_check": "OK", 'model_version': model_version}
 
-
-def predict_pipeline(text):
-    pred = model.predict([text])
-    return classes[pred[0]]
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    language = predict_pipeline(payload.text)
+    return {"language": language}
---
+++ dependency_check.py
@@ -0,0 +1,8 @@
+import sklearn
+print(sklearn.__version__)
+import pandas
+print(pandas.__version__)
+import numpy
+print(numpy.__version__)
+import dill
+print(dill.__version__)
